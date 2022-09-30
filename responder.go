c30a9702d82c33a997bc949287fabb0d7ef341ff
Jeremy-Laier <jeremylaier@outlook.com>
2022-09-30 18:19:16 -0400

    flushing works

--- responder.go
+++ responder.go
@@ -1,41 +1,89 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
+// atomic variable
+var acceptingRequests int32
 
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	var curTime = time.Now()
-	var respJson map[string]interface{}
+type RequestEvent struct {
+	request http.Request
+	writer http.ResponseWriter
+}
 
-	err := json.NewDecoder(r.Body).Decode(&respJson)
-
-	if err != nil {
-		log.Fatalln("could not parse json", err, respJson)
+func processor(requestsChan chan RequestEvent) {
+	for req := range requestsChan {
+		log.Default().Println("event processed", req.request.Method)
 	}
+}
 
+func mimicWrapper(requestChan chan RequestEvent) http.Handler {
+	fn := func (w http.ResponseWriter, r *http.Request) {
+		if acceptingRequests != 0 {
+			w.WriteHeader(500)
+			log.Default().Fatal("closing SERVER")
+			return
+		}
 
-	respJson["receieved"] = curTime.Local().String()
-	jsonResp, err := json.Marshal(respJson)
+		event := RequestEvent{request: *r, writer: w}
+		requestChan <- event
 
-	if err != nil {
-		log.Fatalln("could not reencode json", err)
+		var curTime = time.Now()
+		var respJson map[string]interface{}
+
+		err := json.NewDecoder(event.request.Body).Decode(&respJson)
+
+		if err != nil {
+			log.Fatalln("could not parse json", err, respJson)
+		}
+
+		respJson["receieved"] = curTime.Local().String()
+		jsonResp, err := json.Marshal(respJson)
+
+		if err != nil {
+			log.Fatalln("could not reencode json", err)
+		}
+
+		event.writer.Header().Set("Content-Type", "application/json")
+		event.writer.WriteHeader(200)
+		event.writer.Write(jsonResp)
 	}
-
-	log.Default().Println(string(jsonResp))
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(200)
-	w.Write(jsonResp)
+	return http.HandlerFunc(fn)
 }
 
 func main() {
-	http.HandleFunc("/", getRoot)
+	requests := make(chan RequestEvent, 100)
+	atomic.AddInt32(&acceptingRequests, 0)
+	go processor(requests)
 
-	err := http.ListenAndServe(":3333", nil)
-	fmt.Println(err)
+	mux := http.NewServeMux()
+	handler := mimicWrapper(requests)
+	mux.Handle("/", handler)
+
+	handleCtrlC(requests)
+	err := http.ListenAndServe(":3333", mux)
+	println(err.Error())
 }
+
+// this function handles the sig int and sig term signals
+func handleCtrlC(requestsChan chan RequestEvent) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT)
+
+	go func() {
+		sig := <- sigs
+		atomic.AddInt32(&acceptingRequests, 1)
+		log.Default().Println("signal found closing program, flushing requests", sig)
+
+		os.Exit(1)
+	}()
+}
+
